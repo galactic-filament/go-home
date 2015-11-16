37eaff10a383baa950e2d32746e0eb38094afbe7
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-11-15 21:00:25 -0500

    Add json-reflection endpoint.

--- app/app.go
+++ app/app.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"net/http"
 )
 
-func add(x int, y int) int {
-	return x + y
+type greeting struct {
+	Greeting string `json:"greeting"`
 }
+
+type customReader struct {
+	*bytes.Buffer
+}
+
+func (r customReader) Close() error { return nil }
 
 func getHandler() *mux.Router {
 	r := mux.NewRouter()
@@ -19,13 +28,44 @@ func getHandler() *mux.Router {
 	r.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "Pong")
 	})
+	r.HandleFunc("/reflection", func(w http.ResponseWriter, req *http.Request) {
+		var greeting greeting
+		if err := json.NewDecoder(req.Body).Decode(&greeting); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Could not decode request body")
+			return
+		}
+
+		if err := json.NewEncoder(w).Encode(greeting); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Could not encode response body")
+			return
+		}
+	})
 	return r
 }
 
 func loggingMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		// read the request body for logging
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"url": req.URL,
+				"err": err.Error(),
+			}).Error("Could not read request body")
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Could not read request body")
+			return
+		}
+
+		// re-adding the request body
+		req.Body = customReader{bytes.NewBuffer(body)}
+
+		// passing onto the next middleware
 		log.WithFields(log.Fields{
-			"url": req.URL,
+			"url":  req.URL,
+			"body": string(body),
 		}).Info("Url hit")
 		h.ServeHTTP(w, req)
 	})
--- app/app_test.go
+++ app/app_test.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
-
-func TestAdd(t *testing.T) {
-	assert.Equal(t, 4, add(2, 2), "Add failed!")
-}
 
 func TestHomepage(t *testing.T) {
 	// fetching the request router
@@ -43,4 +41,31 @@ func TestPing(t *testing.T) {
 	// asserting that it worked properly
 	assert.Equal(t, http.StatusOK, w.Code, "Response code was not 200")
 	assert.Equal(t, "Pong", w.Body.String())
+}
+
+func TestReflection(t *testing.T) {
+	// fetching the request router
+	r := getHandler()
+
+	// generating a request payload
+	requestGreeting := greeting{Greeting: "Hello, world!"}
+	payload, err := json.Marshal(requestGreeting)
+	assert.Nil(t, err, "Could not marshal greeting")
+
+	// generating a request to test it
+	req, err := http.NewRequest("POST", "/reflection", bytes.NewBuffer(payload))
+	assert.Nil(t, err, "Could not create new POST /reflection request")
+
+	// serving up a single request and recording the response
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	// asserting that it worked properly
+	assert.Equal(t, http.StatusOK, w.Code, "Response code was not 200")
+
+	// aserting that the request and response match
+	var responseGreeting greeting
+	err = json.NewDecoder(w.Body).Decode(&responseGreeting)
+	assert.Nil(t, err, "Could not decode response body")
+	assert.Equal(t, requestGreeting.Greeting, responseGreeting.Greeting)
 }
